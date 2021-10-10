a0cf3db9ab2e4aee5f5c827052ab6bce95b452e6
Doug Peterson <douglaspeterson6@gmail.com>
2021-10-10 16:24:32 -0500

    created first question to readme generator

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-{
-	"title": "Readme Generator"
-}
+# Readme Generator
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// TODO: Include packages needed for this application
-
+// Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const gm = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [
+const questions =[
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the project title?'
+        message: 'What is the title of the project title?',
+        default: 'Project Title'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the project?',
+        default: 'Project Description'
     }
 ];
 
@@ -15,21 +22,17 @@ const questions = [
 function writeToFile(fileName, data) {
 
     return fs.writeFileSync(fileName, data, function(err) {
-
         if (err) {
           return console.log(err);
         }
-    
         console.log("Success!");
-    
       });
 }
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(function(data) {
-        writeToFile("README.md", data);
-    });
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    writeToFile("README.md", gm(answers));
 }
 
 // Function call to initialize app
